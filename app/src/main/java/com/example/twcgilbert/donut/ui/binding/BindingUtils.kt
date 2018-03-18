0e905cecff809cafdc0846655a424f15509484b7
Tom Gilbert <tom@alteredworlds.com>
2018-03-18 21:16:55 +0000

    ViewModel shouldn't need reference to View

--- app/src/main/java/com/example/twcgilbert/donut/ui/binding/BindingUtils.kt
+++ app/src/main/java/com/example/twcgilbert/donut/ui/binding/BindingUtils.kt
@@ -1,6 +1,8 @@
 package com.example.twcgilbert.donut.ui.binding
 
 import android.databinding.BindingAdapter
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
 import com.example.twcgilbert.donut.ui.widget.CreditScoreDonut
 
 /**
@@ -10,4 +12,12 @@ import com.example.twcgilbert.donut.ui.widget.CreditScoreDonut
 @BindingAdapter("progress", "max")
 fun setProgressMax(creditScoreDonut: CreditScoreDonut, value: Int, max: Int) {
     creditScoreDonut.setProgressAndMax(value, max)
+}
+
+@BindingAdapter("snackBarText")
+fun setSnackbarText(coordinatorLayout: CoordinatorLayout, text: String?) {
+    if ((null != text) && text.isNotEmpty()) {
+        Snackbar.make(coordinatorLayout, text, Snackbar.LENGTH_LONG)
+                .show();
+    }
 }
